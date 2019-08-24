fae595a6400387957211b96b2dfe25a7c945654c
LGTM Test 1 <semmletestuser+lgtmtest1@gmail.com>
2019-08-24 15:38:55 +0000

    Add an alert.

--- code/java.java
+++ code/java.java
@@ -5,8 +5,10 @@ public class java {
 		for (int i=0; i < ints.length; i++) {
 			int x = ints[i+1]; // true
 			int y = ints[i+1]; // lgtm[java/index-out-of-bounds] false
+			int z = ints[i+1]; // true
 			System.out.println(x);
 			System.out.println(y);
+			System.out.println(z);
 		}
 	}
 }
